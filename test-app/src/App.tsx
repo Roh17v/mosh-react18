376c7b9422777850dde21267baba918cfd77c17d
Rohit Verma <137288929+Roh17v@users.noreply.github.com>
2023-11-13 10:58:49 +0000

    css module for button

--- test-app/src/App.tsx
+++ test-app/src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Alert from "./components/Alert";
-import Button from "./components/Button";
+import Button from "./components/Button/Button";
 import ListGroup from "./components/ListGroup/ListGroup";
 import "./App.css";
 import { AiFillLike } from "react-icons/ai";
@@ -8,7 +8,11 @@ import { AiFillLike } from "react-icons/ai";
 function App() {
   const [alertVisibility, setAlertVisibility] = useState(false);
   const list = ["Tokyo", "Delhi", "Paris"];
-  return <AiFillLike color="blue" size="30" />;
+  return (
+    <Button color="primary" onClick={() => console.log("clicked")}>
+      My Button
+    </Button>
+  );
 }
 
 export default App;
